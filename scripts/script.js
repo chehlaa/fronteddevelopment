53e1594ede0c5db3acec25f3ee7d99c535540410
chehlaa <70895452+chehlaa@users.noreply.github.com>
2020-11-04 23:08:22 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
-function myFunction() {
-  document.body.style.backgroundColor = "black";
+//variables darkmode: 
+
+var darkmodeButton = document.querySelector('#buttondark');
+var bodyelement = document.querySelector('body');
+
+//functions 
+function darkmodusbutton(event) {
+  bodyelement.classList.toggle('buttondark');
 }
+
+//eventlisteners
+darkmodeButton.addEventListener('click', darkmodusbutton);
